b72235efb9f52612683987a4df484dd35960a76b
Graham Morby <grahammorbydev@gmail.com>
2022-05-20 09:17:48 +0100

    search working and page displaying data

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+import whois
+import datetime
 
 app = Flask(__name__)
-
+app.debug = True
+app.config['TEMPLATE_AUTO_RELOAD'] = True
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/search', methods=['POST'])
+def search():
+    url = request.form['url']
+    w = whois.whois(url)
+    w.expiration_date  # dates converted to datetime object
+    datetime.datetime(2013, 6, 26, 0, 0)
+    w.text
+    return render_template('search.html', url=url, record=w)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000, host='127.0.0.1')
